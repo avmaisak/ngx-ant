1d7d151295ef38a493b2a59a2990111357a01c39
Anton Maisak <anton@maisak.ru>
2020-04-12 16:08:05 +0300

    fix interface name fix routing -> now directly to demo module

--- src/app/demo/services/http.service.ts
+++ src/app/demo/services/http.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { IFilter } from '../interfaces/filter.interface';
-import { IPaginatedList } from '../interfaces/paginaled-list.interface';
+import { IPaginatedList } from '../interfaces/paginated-list.interface';
 @Injectable({
   providedIn: 'root'
 })
